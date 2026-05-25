ebf12b93b25c49f5db01eeb262a9e4880e66f752
Dominic Clifton <me@dominicclifton.name>
2026-05-25 19:28:14 +0200

    Add an ESP32C6 blinky.

---
+++ mcu/esp32c6/blinky/src/main.rs
@@ -0,0 +1,49 @@
+#![no_std]
+#![no_main]
+
+use defmt::{error, info};
+use embassy_executor::Spawner;
+use embassy_time::{block_for, Duration};
+use esp_hal::{
+    clock::CpuClock,
+};
+use esp_hal::gpio::{Level, Output, OutputConfig};
+
+esp_bootloader_esp_idf::esp_app_desc!();
+#[panic_handler]
+fn panic(panic_info: &core::panic::PanicInfo) -> ! {
+    error!("{}", panic_info);
+    loop {}
+}
+
+#[esp_rtos::main]
+async fn main(_spawner: Spawner) {
+    rtt_target::rtt_init_defmt!();
+    defmt::println!("Init!");
+
+    let peripherals = esp_hal::init(esp_hal::Config::default()
+        .with_cpu_clock(CpuClock::max())
+    );
+
+    //let timg0 = esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG0);
+    //let timer0 = esp_hal::timer::systimer::SystemTimer::new(peripherals.SYSTIMER);
+
+    let mut led = Output::new(peripherals.GPIO0, Level::Low, OutputConfig::default());
+
+    let mut counter = 0;
+    loop {
+        if counter == 10 {
+            panic!("test panic");
+        }
+        defmt::println!("test println");
+        led.toggle();
+        info!("test info");
+
+        // fails, time driver init missing...
+        //Timer::after(Duration::from_micros(250)).await;
+
+        block_for(Duration::from_micros(250));
+        error!("test error");
+        counter += 1;
+    }
+}
